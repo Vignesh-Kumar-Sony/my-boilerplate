1f8463ad692f86fca599a1685cd6da732e212096
Kumar <Vignesh.Kumar@sony.com>
2024-08-06 10:45:24 +0530

    Added updated code with Zipping functionality

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs-extra'); // Use fs-extra for easy directory copying
+const archiver = require('archiver'); // Include archiver
 
 if (process.argv.length < 3) {
   console.log('You have to provide a name for your app.');
@@ -15,65 +16,80 @@ const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, 'templateOutput', projectName);
 const localFolderPath = path.join(currentPath, 'bin', 'Edge_App_Source_Code/classification-sample'); // Path to your local folder
-let zipFilePath;
+// const localFolderPath = path.join(currentPath, 'bin', 'Edge_App_Source_Code/Object_Detection-sample'); // Path to your local folder
+const zipFilePath = path.join(currentPath, 'output', `${projectName}.zip`);
 
 try {
-  fs.mkdirSync(projectPath)
-// Copy the local folder to the new project directory
+  // Create the project directory
+  fs.mkdirSync(projectPath);
+  // Copy the local folder to the new project directory
   fs.copySync(localFolderPath, projectPath);
   console.log(`Copied ${localFolderPath} to ${projectPath}`);
 } catch (err) {
   if (err.code === 'EEXIST') {
-    console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+    console.log(`The directory ${projectName} already exists in the current directory, please give it another name.`);
   } else {
     console.log(err);
   }
   process.exit(1);
 }
 
+async function zipDirectory(source, out) {
+  const archive = archiver('zip', { zlib: { level: 9 }});
+  const stream = fs.createWriteStream(out);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on('error', err => reject(err))
+      .pipe(stream);
+
+    stream.on('close', () => resolve());
+    archive.finalize();
+  });
+}
+
 async function main() {
   try {
+
+    //     //     console.log('Downloading files...');
+//     //     // var exeCommand = `git clone --depth 1 ${git_repo} ${projectPath}`
+//     //     var exeCommand = `git clone ${git_repo} ${projectPath}`
+//     //     // var exeCommand = `zip -r my-folder.zip /path/to/your/project/folder-to-clone`
+//     //     console.log(currentPath, projectName)
+//     //     console.log(exeCommand)
+//     //     execSync(exeCommand);
     
-    //     console.log('Downloading files...');
-    //     // var exeCommand = `git clone --depth 1 ${git_repo} ${projectPath}`
-    //     var exeCommand = `git clone ${git_repo} ${projectPath}`
-    //     // var exeCommand = `zip -r my-folder.zip /path/to/your/project/folder-to-clone`
-    //     console.log(currentPath, projectName)
-    //     console.log(exeCommand)
-    //     execSync(exeCommand);
+//     //     console.log("pROJECTpATH"+projectPath)
+//     process.chdir(projectPath);
+
+// //     // Executable code
+// //     // console.log('Initializing npm...');
+// //     // execSync('npm init -y', { stdio: 'inherit' });
     
-    //     console.log("pROJECTpATH"+projectPath)
+// //     // console.log('Installing dependencies...');
+// //     // execSync('npm install');
+// //     // console.log('packages installed');
+
+// //     // console.log('Removing useless files');
+// //     // execSync('npx rimraf ./.git');
+// //     // console.log('rimraf installed');
+
+
+//     // console.log('Initializing build...');
+//     // execSync('cmake -Bbuild', { stdio: 'inherit' });
+
+//     // console.log('Installing dependencies...');
+//     // execSync('cmake --build build', { stdio: 'inherit' });
+
+//     // console.log('The installation is done, this is ready to use!');
+//     // console.log('To Run executable file use "./build/TimeStub" CLI command');
     process.chdir(projectPath);
 
-//     // Executable code
-//     // console.log('Initializing npm...');
-//     // execSync('npm init -y', { stdio: 'inherit' });
-    
-//     // console.log('Installing dependencies...');
-//     // execSync('npm install');
-//     // console.log('packages installed');
-
-//     // console.log('Removing useless files');
-//     // execSync('npx rimraf ./.git');
-//     // console.log('rimraf installed');
-
-
-    // console.log('Initializing build...');
-    // execSync('cmake -Bbuild', { stdio: 'inherit' });
-
-    // console.log('Installing dependencies...');
-    // execSync('cmake --build build', { stdio: 'inherit' });
-
-    // console.log('The installation is done, this is ready to use!');
-    // console.log('To Run executable file use "./build/TimeStub" CLI command');
-
+    // Zip the project directory
     console.log('Zipping the project directory...');
-    console.log("projectName"+projectName+"   currentPath"+projectName);
-    execSync(`zip -r ${projectName}.zip .`, { cwd: projectPath });
-    // execSync(`zip -r ${projectName}.zip  ../templateOutputZip`);
-    console.log(`Created zip file: ${path.join(currentPath, `${projectName}.zip`)}`);
-    zipFilePath = path.join(currentPath, `templateOutput/${projectName}/${projectName}.zip`)
-    console.log(zipFilePath);
+    await zipDirectory(projectPath, zipFilePath);
+    console.log(`Created zip file: ${zipFilePath}`);
   } catch (error) {
     console.log(error);
   }
